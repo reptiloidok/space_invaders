0d836254b5679c083799bf450ad9cff0ee433f1d
unknown <alexeevaoly57@gmail.com>
2023-10-09 23:30:23 +0300

    add new enemy and game over screen

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import pygame as pg
 import random
 
-
 pg.init()
 
 screen_width, screen_height = 800, 600
 
-FPS = 24    # frame per second
+FPS = 24  # frame per second
 clock = pg.time.Clock()
 
 # изображения
-bg_img = pg.image.load('No_thanks/src/background.png')
-icon_img = pg.image.load('No_thanks/src/ufo.png')
+bg_img = pg.image.load('space_invaders/src/background.png')
+icon_img = pg.image.load('space_invaders/src/ufo.png')
 
 display = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_icon(icon_img)
 pg.display.set_caption('Космическое вторжение')
 
 sys_font = pg.font.SysFont('arial', 34)
-font = pg.font.Font('No_thanks/src/04B_19.TTF', 48)
+font = pg.font.Font('space_invaders/src/04B_19.TTF', 48)
 
 # display.fill('blue', (0, 0, screen_width, screen_height))
-display.blit(bg_img, (0, 0))        # image.tr
+display.blit(bg_img, (0, 0))  # image.tr
 
 text_img = sys_font.render('Score 123', True, 'white')
 # display.blit(text_img, (100, 50))
@@ -31,37 +30,45 @@ w, h = game_over_text.get_size()
 # display.blit(game_over_text, (screen_width/2 - w/2, screen_height / 2 - h/2))
 
 # игрок
-player_img = pg.image.load('No_thanks/src/player.png')
+player_img = pg.image.load('space_invaders/src/player.png')
 player_width, player_height = player_img.get_size()
 player_gap = 10
 player_velocity = 10
 player_dx = 0
-player_x = screen_width/2 - player_width/2
-player_y = screen_height  - player_height - player_gap
+player_x = screen_width / 2 - player_width / 2
+player_y = screen_height - player_height - player_gap
 
 # пуля
-bullet_img = pg.image.load('No_thanks/src/bullet.png')
+bullet_img = pg.image.load('space_invaders/src/bullet.png')
 bullet_width, bullet_height = bullet_img.get_size()
-bullet_dy = -5
-bullet_x = 0     # микро дз - пускать из середины
+bullet_dy = -10
+bullet_x = 0  # микро дз - пускать из середины
 bullet_y = 0
-bullet_alive = False    # есть пуля?
+bullet_alive = False  # есть пуля?
+
+# лазер
+
 
 # противник
-enemy_img = pg.image.load('No_thanks/src/enemy.png')
+enemy_img = pg.image.load('space_invaders/src/enemy.png')
+angry_enemy_img = pg.image.load('space_invaders/src/enemy2.png')
 enemy_width, enemy_height = enemy_img.get_size()
-enemy_dx = 0
-enemy_dy = 1
+enemy_dx = 10
+enemy_dy = 3
 enemy_x = 0
 enemy_y = 0
+flag_wall = True
+
+# подсчёт очков
+score = 0
+
 
 def enemy_create():
     """ Создаем противника в случайном месте вверху окна."""
     global enemy_y, enemy_x
-    enemy_x = random.randint(0, screen_width - enemy_width)   # screen_width / 2 - enemy_width / 2
+    enemy_x = random.randint(0, screen_width - enemy_width)  # screen_width / 2 - enemy_width / 2
     enemy_y = 0
     print(f'CREATE: {enemy_x=}')
-
 
 
 def model_update():
@@ -69,15 +76,17 @@ def model_update():
     bullet_model()
     enemy_model()
 
+
 def palayer_model():
-    x = 7   # создание переменной и ее инициализация
-    x = 7   # изменение значения уже созданной переменнной
+    x = 7  # создание переменной и ее инициализация
+    x = 7  # изменение значения уже созданной переменнной
     global player_x
     player_x += player_dx
     if player_x < 0:
         player_x = 0
     elif player_x > screen_width - player_width:
         player_x = screen_width - player_width
+
 
 def bullet_model():
     """ Изменяется положение пули.
@@ -88,19 +97,27 @@ def bullet_model():
     if bullet_y < 0:
         bullet_alive = False
 
+
 def bullet_create():
     global bullet_y, bullet_x, bullet_alive, bullet_width
     bullet_alive = True
-    bullet_x = player_x+bullet_width/2
+    bullet_x = player_x + bullet_width / 2
     bullet_y = player_y - bullet_height
+
 
 def enemy_model():
     """ Изменение положения противника, рассчет поражений."""
-    global enemy_y, enemy_x, bullet_alive
-
-    enemy_x += enemy_dx
+    global enemy_y, enemy_x, bullet_alive, score, flag_wall
+    if enemy_x + enemy_width > screen_width or enemy_x <= 0:
+        flag_wall = not flag_wall
     enemy_y += enemy_dy
-    if enemy_y > screen_height:
+    if score >= 200:
+        if flag_wall:
+            enemy_x += enemy_dx
+        else:
+            enemy_x -= enemy_dx
+    if enemy_y + enemy_height > screen_height:
+        game_over()
         enemy_create()
 
     # пересечение с пулей
@@ -110,17 +127,42 @@ def enemy_model():
         is_crossed = re.colliderect(rb)
         # попал!
         if is_crossed:
+            score += 100
             print('BANG!')
             enemy_create()
             bullet_alive = False
 
+
 def display_redraw():
+    global score
     display.blit(bg_img, (0, 0))
     display.blit(player_img, (player_x, player_y))
-    display.blit(enemy_img, (enemy_x, enemy_y))
+    if score < 200:
+        display.blit(enemy_img, (enemy_x, enemy_y))
+    else:
+        display.blit(angry_enemy_img, (enemy_x, enemy_y))
+    score_img = sys_font.render('SCORE  ' + str(score), True, 'white')
+    display.blit(score_img, (0, 0))
     if bullet_alive:
         display.blit(bullet_img, (bullet_x, bullet_y))
     pg.display.update()
+
+
+def game_over():
+    display_redraw()
+    global score
+    over_img = sys_font.render('GAME OVER ', True, 'white')
+    display.blit(over_img, ((screen_width - over_img.get_size()[0]) / 2, 250))
+    score_img = sys_font.render('SCORE  ' + str(score), True, 'white')
+    display.blit(score_img, ((screen_width - score_img.get_size()[0]) / 2, 300))
+    text_img = sys_font.render('press any button to continue', True, 'white')
+    display.blit(text_img, ((screen_width - text_img.get_size()[0]) / 2, 350))
+    pg.display.update()
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                return
+
 
 def event_processing():
     global player_dx
@@ -145,14 +187,14 @@ def event_processing():
 
         # по левому клику мыши стреляем
         if event.type == pg.MOUSEBUTTONDOWN:
-            key = pg.mouse.get_pressed()    # key[0] - left, key[2] - right
+            key = pg.mouse.get_pressed()  # key[0] - left, key[2] - right
             print(f'{key[0]=} {bullet_alive=}')
             if not bullet_alive:
                 bullet_create()
 
-
     clock.tick(FPS)
     return running
+
 
 # random.seed(77)
 enemy_create()
